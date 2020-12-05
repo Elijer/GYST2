61f751796788ac395bc3d27efe95bd31fde573e3
Jah <kennedye2@vcu.edu>
2020-12-04 23:09:44 -0500

    running a pretty basic query and returning a document

--- helpers/findGame.js
+++ helpers/findGame.js
@@ -1,3 +1,17 @@
-export function findGame(){
-    
+export function findGame(db){
+    var matches = db.collection("pending");
+    var query = matches.where("player2", "!=", null).limit(1);
+
+    query.get()
+    .then(function(querySnapshot){
+        console.log(querySnapshot);
+        if (querySnapshot.empty){
+            console.log("No games found");
+        } else {
+            console.log("NOt empty");
+            querySnapshot.forEach(function(doc) {
+                console.log(doc.data());
+            })
+        }
+    })
 }
