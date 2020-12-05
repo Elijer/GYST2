57c5175b412e5c096491c0858d2fa5befbd61e18
Jah <kennedye2@vcu.edu>
2020-12-04 22:54:50 -0500

    added begining of findGame() function in its own module/file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import 'firebase/firestore';
 import { firebaseConfig } from "./helpers/firebaseConfig";
 import { handleEmulators } from "./helpers/handleEmulators";
 import { gameplay } from "./helpers/gameplay";
+import { findGame } from './helpers/findGame';
 
 
 
@@ -11,15 +12,7 @@ document.addEventListener("DOMContentLoaded", event => {
     firebase.initializeApp(firebaseConfig);
     var db = firebase.firestore();
     handleEmulators(db);
-
-
-    // Local Emulators Test
-    var citiesRef = db.collection("firestore-collection");
-    citiesRef.doc("firestore-documsent").set({
-        name: "Some document", state: "VA", country: "USsssZZZZsA?",
-        capital: false,
-        ridesBicycles: "sure",
-        regions: ["west_coast", "norcal"]
-    });
+    findGame(db);
+    //gameplay();
 
 });
