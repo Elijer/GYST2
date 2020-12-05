cafdda123cba8d4112305f48a3dc820482995b3d
Jah <kennedye2@vcu.edu>
2020-12-04 23:01:10 -0500

    added an eent listener for findGame()

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ document.addEventListener("DOMContentLoaded", event => {
     firebase.initializeApp(firebaseConfig);
     var db = firebase.firestore();
     handleEmulators(db);
-    findGame(db);
     //gameplay();
+
+    //// EVENT LISTENERS
+    document.getElementById('find-game').addEventListener('click', function(){
+        findGame(db);
+    });
 
 });
