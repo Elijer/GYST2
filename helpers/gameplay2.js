3d179030e66c8eb932224accbe0c54b2d8f99569
Jah <kennedye2@vcu.edu>
2020-12-21 22:08:14 -0500

    board is renendering

--- helpers/gameplay2.js
+++ helpers/gameplay2.js
@@ -5,6 +5,7 @@ import { renderBoard } from "./renderboard";
 // Show the messages
 show("message-content");
 show("message");
+hide("welcome");
 
 export function gameplay2(board){
 
@@ -12,6 +13,40 @@ export function gameplay2(board){
     var say = function(t){
         var target = document.getElementById("message-content");
         target.innerHTML = t;
+    }
+
+    var renderBoard = function(callback){
+
+        // delete previous board to make room for new one
+        document.getElementById("grid-container").innerHTML = "";
+    
+        for (var i = 0; i < board.length; i++) {
+            for (var j = 0; j < board[0].length; j++){
+        
+                const item = document.createElement('div');
+                item.className = "sq";
+                item.id = "sq" + i + j;
+                item.innerHTML = board[i][j];
+        
+                let row = i;
+                let col = j;
+        
+                item.addEventListener("click", function(){
+
+                if (callback){
+                    callback();
+                } else {
+                    console.log("board is locked");
+                }
+                })
+        
+                document.getElementById("grid-container").appendChild(item);
+        
+            }
+        }
+        
+        say("Your Move!")
+
     }
 
     renderBoard();
