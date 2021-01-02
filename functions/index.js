77c3491e7d6d102c62cbf2637c8fe1330b8475d9
Jah <kennedye2@vcu.edu>
2021-01-02 12:35:36 -0500

    event fired on client disconnect

--- functions/index.js
+++ functions/index.js
@@ -15,7 +15,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-  });
+
+    socket.on('disconnect', function() {
+        console.log("user disconnected")
+    })
+});
 
 http.listen(3020, () => {
   console.log('listening on *:3020');
